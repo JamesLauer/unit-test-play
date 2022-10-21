1c895ab6645c07bac210aea83df61aea6bbae430
jameslauer <james.lauer@outlook.com>
2022-10-21 21:26:52 +1100

    update gh actions

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,2 @@
-# def sum(arg):
-#     total = 0
-#     for val in arg:
-#         total += val
-#     return total
 
 
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,7 +1,4 @@
-# def sum(arg):
-#     total = 0
-#     for val in arg:
-#         total += val
-#     return total
+
+
 
 
